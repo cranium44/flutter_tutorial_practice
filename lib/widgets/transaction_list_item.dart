c6b595b49df6432f4a6ac736568028d8f5456e71
Adabe <adabe@scienta.co>
2020-10-11 22:24:13 +0100

    further style list item

--- lib/widgets/transaction_list_item.dart
+++ lib/widgets/transaction_list_item.dart
@@ -8,43 +8,57 @@ class TransactionItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            margin: EdgeInsets.fromLTRB(5, 5, 15, 5),
-            padding: EdgeInsets.all(10),
-            child: Text(
-              "N " + transaction.ammount.toString(),
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.green,
-              ),
-            ),
-            decoration: BoxDecoration(
-                border: Border.all(width: 1, color: Colors.cyanAccent),
-                borderRadius: BorderRadius.circular(50)),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                child: Text(
-                  transaction.title,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.bold,
-                  ),
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Card(
+        elevation: 4,
+        shadowColor: Colors.limeAccent,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(5, 5, 15, 5),
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "N " + transaction.ammount.toString(),
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
                 ),
-                margin: EdgeInsets.symmetric(vertical: 8),
               ),
-              Text(transaction.date.toString())
-            ],
-          )
-        ],
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.cyanAccent),
+                  borderRadius: BorderRadius.circular(50)),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  child: Text(
+                    transaction.title,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  margin: EdgeInsets.symmetric(vertical: 8),
+                ),
+                Container(
+                  child: Text(
+                    transaction.date.toString(),
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  margin: EdgeInsets.symmetric(vertical: 8),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
