ddf397e85729c0a51753541f5f9ea3a53a3c6b51
Adabe <adabe@scienta.co>
2020-10-14 17:28:03 +0100

    add fonts to theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,25 @@ import 'widgets/screens/home_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.purple
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amberAccent,
+        fontFamily: 'Montserrat',
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                  fontFamily: "OpenSans",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+        ),
       ),
       home: HomePage(),
     );
   }
 }
-
