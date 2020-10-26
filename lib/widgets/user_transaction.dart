7fa05e4e6d552eb114e49f4643525def4c811ee9
Adabe <adabe@scienta.co>
2020-10-26 11:24:04 +0100

    change list item

--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial_practice/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 import 'transaction_list_item.dart';
 
@@ -12,11 +13,14 @@ class Transactions extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 400,
+      padding: EdgeInsets.symmetric(horizontal: 20),
       child: transactions.isEmpty
           ? Column(
               children: [
                 Text("No transactions added yet"),
-                SizedBox(height: 15,),
+                SizedBox(
+                  height: 15,
+                ),
                 Container(
                   height: 300,
                   child: Image.asset(
@@ -28,7 +32,30 @@ class Transactions extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                return TransactionItem(transactions[index]);
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 25,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                          child: Text("\u20A6 ${transactions[index].ammount}"),
+                        ),
+                      ),
+                    ),
+                    title: Text(transactions[index].title),
+                    subtitle: Text(
+                      DateFormat.yMMMEd().format(transactions[index].date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                );
               },
               itemCount: transactions.length,
             ),
